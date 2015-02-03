14727e330ce3c348f3e5d773f69a539559f43d4e
Voidbab <niko.halikka@gmail.com>
2015-02-03 21:49:26 +0200

    more on entity manager

--- EUS/EntityManager.cpp
+++ EUS/EntityManager.cpp
@@ -1,11 +1,30 @@
 #include "EntityManager.h"
 
-
-EntityManager::EntityManager()
-{
+EntityManager::EntityManager() {
 }
 
+#pragma region Public memebers
+void EntityManager::addEntity(Entity* entity) {
+}
+void EntityManager::removeEntity(Entity* entity) {
+}
 
-EntityManager::~EntityManager()
-{
+std::vector<Entity* const> EntityManager::findEntitiesWithTag(std::string tag) const {
+}
+
+Entity* const EntityManager::findEntityWithTag(std::string tag) const {
+}
+Entity* const EntityManager::findEntityWithID(int id) const {
+}
+
+void EntityManager::update() {
+}
+void EntityManager::draw() {
+}
+#pragma endregion
+
+void EntityManager::freeEntities(){
+}
+
+EntityManager::~EntityManager() {
 }
--- EUS/EntityManager.h
+++ EUS/EntityManager.h
@@ -11,11 +11,11 @@ public:
 	EntityManager();
 
 	void addEntity(Entity* entity);
-	void addEntity(Entity* entity);
+	void removeEntity(Entity* entity);
 
 	std::vector<Entity* const> findEntitiesWithTag(std::string tag) const;
-	std::vector<Entity* const> findEntityWithTag(std::string tag) const;
-
+	
+	Entity* const findEntityWithTag(std::string tag) const;
 	Entity* const findEntityWithID(int id) const;
 
 	void update();
